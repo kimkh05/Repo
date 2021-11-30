30412320c695f04a19df9946ee6ffdbcaca377b5
김경호 <nestjs01@naver.com>
2021-11-30 20:02:10 +0900

    📃:: 로그인 구현

--- pro/src/App.js
+++ pro/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css'; 
-import React, { useState, useEffect } from "react";
-import LoginMenu from './loginMenu';
-import * as S from './components/style';
+import "./App.css";
+import React from "react";
+import LoginMenu from "./loginMenu";
+import InfinityScroll from "./infinityScroll";
 
 function App() {
   return (
     <>
       <LoginMenu />
+      <InfinityScroll />
     </>
   );
 }
--- pro/src/loginMenu.js
+++ pro/src/loginMenu.js
@@ -1,31 +1,63 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as S from "./components/style";
 
-const header = () => {
-  const [data, setData] = useState("");
-  console.log(data);
-  useEffect(() => {
-    axios({
-      method: "post",
-      url: "http://3.36.161.36/",
-    })
+function LoginMenu() {
+  const [inputId, setInputId] = useState(""); // 동적 사용을 위해 useState 사용
+  const [inputPw, setInputPw] = useState("");
+  const BASEURL = process.env.REACT_APP_BASE_URL;
+
+  console.log(BASEURL);
+
+  const clickId = (e) => {
+    // id input 클릭했을때 실행
+    setInputId(e.target.value); // input 값 변경
+  };
+
+  const clickPw = (e) => {
+    setInputPw(e.target.value); // input 값 변경
+  };
+
+  const clickLogin = () => {
+    console.log(inputId); // input 값 확인용
+    console.log(inputPw); // input 값 확인용
+    axios
+      .post(`${BASEURL}/login/admin`, {
+        id: inputId,
+        password: inputPw,
+      })
       .then((res) => {
-        console.log(res);
-        setData(res.data);
+        alert("로그인 성공!");
+        console.log(res); 
       })
       .catch((err) => {
         console.log(err);
+        alert("Id와 Password를 확인해주세요!");
       });
-  }, []);
+  };
+
   return (
     <S.login>
       <S.title>로그인</S.title>
-      <S.idInput type="id" name="id" placeholder="ID 입력" />
-      <S.pwdInput type="password" name="password" placeholder="password 입력" />
-      <S.btn>로그인</S.btn>
+      <S.idInput
+        type="id"
+        name="id"
+        placeholder="Id 입력"
+        value={inputId}
+        onChange={clickId}
+      />
+      <S.pwdInput
+        type="password"
+        name="password"
+        placeholder="Password 입력"
+        value={inputPw}
+        onChange={clickPw}
+      />
+      <S.btn type="submit" onClick={clickLogin}>
+        로그인
+      </S.btn>
     </S.login>
   );
-};
+}
 
-export default header;
+export default LoginMenu;
